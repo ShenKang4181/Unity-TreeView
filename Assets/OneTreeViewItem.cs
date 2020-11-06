f59fbf9b9bf9bcb8f67b08ab5567d77e9df05108
ShenKang4181 <50401776+ShenKang4181@users.noreply.github.com>
2020-11-06 11:22:05 +0800

    Add files via upload

---
+++ Assets/OneTreeViewItem.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OneTreeViewItem : MonoBehaviour
+{
+    /// <summary>
+    /// 头缩进
+    /// </summary>
+    public RectTransform startSuoJin;
+
+    /// <summary>
+    /// 收缩
+    /// </summary>
+    public Button btnShouSuo;
+
+    public List<GameObject> closes = new List<GameObject>();
+
+    public IEnumerator Start()
+    {
+        yield return new WaitForEndOfFrame();
+        var index = transform.GetSiblingIndex() + 1;
+        bool isCanSuoJin = index < transform.parent.childCount &&
+            transform.parent.GetChild(index).GetComponent<OneTreeViewItem>().
+            startSuoJin.sizeDelta.x > startSuoJin.sizeDelta.x;
+        if (isCanSuoJin)
+        {
+            btnShouSuo.onClick.AddListener(OnClickBtnShouSuo);
+        }
+        else
+        {
+            btnShouSuo.GetComponent<Graphic>().color = Color.clear;
+            btnShouSuo.GetComponent<Graphic>().raycastTarget = false;
+        }
+    }
+
+    public void SetStartSuoJin(Vector2 suoJin)
+    {
+        startSuoJin.sizeDelta = suoJin;
+    }
+
+    public void OnClickBtnShouSuo()
+    {
+        if (closes.Count != 0)
+        {
+            for (int i = 0; i < closes.Count; i++)
+            {
+                closes[i].gameObject.SetActive(true);
+            }
+            closes.Clear();
+            return;
+        }
+
+        bool b = IsNextSiblingActive();
+        var index = transform.GetSiblingIndex() + 1;
+        while (index < transform.parent.childCount)
+        {
+            if (transform.parent.GetChild(index).GetComponent<OneTreeViewItem>().
+                startSuoJin.sizeDelta.x > startSuoJin.sizeDelta.x)
+            {
+                if (b && transform.parent.GetChild(index).gameObject.activeSelf )
+                {
+                    closes.Add(transform.parent.GetChild(index).gameObject);
+                }
+
+                transform.parent.GetChild(index).gameObject.SetActive(!b);
+            }
+            else
+            {
+                break;
+            }
+            index++;
+        }
+    }
+
+    public bool IsNextSiblingActive()
+    {
+        var index = transform.GetSiblingIndex() + 1;
+        if (index < transform.parent.childCount)
+        {
+            return transform.parent.GetChild(index).gameObject.activeSelf;
+        }
+        throw new Exception();
+    }
+}
---
+++ Assets/PanelTreeViewList.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanelTreeViewList : MonoBehaviour
+{
+    /// <summary>
+    /// 间距
+    /// </summary>
+    public int jianJv = 30;
+
+    public GameObject prefabOneTreeViewItem;
+    public Transform content;
+
+    void Start()
+    {
+        CreateOneTreeViewItem(0);
+        CreateOneTreeViewItem(1);
+        CreateOneTreeViewItem(2);
+        CreateOneTreeViewItem(3);
+        CreateOneTreeViewItem(3);
+        CreateOneTreeViewItem(3);
+        CreateOneTreeViewItem(2);
+        CreateOneTreeViewItem(3);
+        CreateOneTreeViewItem(0);
+        CreateOneTreeViewItem(1);
+        CreateOneTreeViewItem(2);
+        CreateOneTreeViewItem(3);
+        CreateOneTreeViewItem(2);
+        CreateOneTreeViewItem(3);
+        CreateOneTreeViewItem(3);
+        CreateOneTreeViewItem(3);
+        CreateOneTreeViewItem(0);
+    }
+
+    public void CreateOneTreeViewItem(int layer)
+    {
+        var obj = Instantiate(prefabOneTreeViewItem, content);
+        if (layer != 0)
+        {
+            obj.GetComponent<OneTreeViewItem>().SetStartSuoJin(new Vector2(jianJv * layer, 0));
+        }
+    }
+}
